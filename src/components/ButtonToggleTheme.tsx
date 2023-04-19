1d4a85bcde9ded7aefaa6adbb0e76064168955b6
Wojtek Gorecki <hallo@wojtek-gorecki.de>
2023-04-19 15:29:56 +0200

    Add useCallback to label function in toggle button

--- src/components/ButtonToggleTheme.tsx
+++ src/components/ButtonToggleTheme.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+
 interface ButtonToggleThemeProps {
   theme: boolean,
   onClick(): void
@@ -5,12 +7,11 @@ interface ButtonToggleThemeProps {
 
 function ButtonToggleTheme({ theme, onClick }: ButtonToggleThemeProps) {
 
-  const getButtonLabel = (): string => {
+  const getButtonLabel = useCallback((): string => {
     return theme ? 'Light' : 'Dark';
-  }
+  }, [theme]);
 
   return <span className="ButtonToggleTheme" onClick={onClick}>{getButtonLabel()}</span>;
-
 }
 
 export default ButtonToggleTheme;
